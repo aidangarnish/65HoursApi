baa6f12feeb1756743640e7b00896fa64dee12ff
Aidan Garnish <garnish@outlook.com>
2016-01-14 16:02:31 +0000

    Services project added

---
+++ 65Hours.Services/Interfaces/ISkillService.cs
@@ -0,0 +1,16 @@
+﻿using _65Hours.Models;
+using _65Hours.Models.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _65Hours.Services.Interfaces
+{
+    public interface ISkillService
+    {
+        ResultT<Skill> All();
+        ResultT<Skill> GetById(int id);
+    }
+}
---
+++ 65Hours.Services/SkillService.cs
@@ -0,0 +1,24 @@
+﻿using _65Hours.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _65Hours.Models;
+using _65Hours.Models.Results;
+
+namespace _65Hours.Services
+{
+    public class SkillService : ISkillService
+    {
+        public ResultT<Skill> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResultT<Skill> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
