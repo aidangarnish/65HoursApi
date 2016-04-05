11e1746023ce4d5f14b193d25b89051ab1c03ccb
Aidan Garnish <garnish@outlook.com>
2016-04-05 14:37:53 +0100

    Userskill controller added - method to save userSkill

---
+++ 65HoursApi/Controllers/UserSkillController.cs
@@ -0,0 +1,39 @@
+﻿using _65Hours.Models;
+using _65Hours.Models.Results;
+using _65Hours.Services.Interfaces;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _65HoursApi.Controllers
+{
+    [RoutePrefix("api/UserSkill")]
+    [Authorize]
+    public class UserSkillController : Controller
+    {
+        private IUserSkillService _userSkillService;
+        private ApplicationUserManager _userManager;
+        public UserSkillController(IUserSkillService userSkillService)
+        {
+            _userSkillService = userSkillService;
+            _userManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+        }
+        // GET: UserSkill
+        public async Task<ResultT<UserSkill>>  Save(UserSkill userSkill)
+        {
+            var result = new ResultT<UserSkill>();
+
+            ApplicationUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            userSkill.UserId = user.Id;
+
+            _userSkillService.Save(userSkill);
+
+            return result;
+        }
+    }
+}
